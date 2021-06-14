6cf1070f9701e33cb492a682a85bbb60f6ec1e28
geezylucas <sorec@live.com>
2021-06-14 17:01:23 -0500

    Initial project

---
+++ src/main/java/com/geezylucas/app/EmpleadosAppApplication.java
@@ -0,0 +1,13 @@
+package com.geezylucas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleadosAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpleadosAppApplication.class, args);
+    }
+
+}
